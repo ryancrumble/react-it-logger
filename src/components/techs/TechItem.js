4987c8f59612c94cd634bef474a0eec8c3eb73b7
Ryan <ryan@pop-os.localdomain>
2019-06-29 11:30:49 +0200

    initial commit

---
+++ src/components/techs/TechItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { deleteTech } from '../../actions/techActions';
+
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const TechItem = ({ tech: { firstName, lastName, id }, deleteTech }) => {
+  const onDelete = () => {
+    deleteTech(id);
+    M.toast({ html: 'Technician deleted' });
+  };
+
+  return (
+    <li className='collection-item'>
+      <div>
+        {firstName} {lastName}
+        <a href='#!' className='secondary-content'>
+          <i className='material-icons grey-text' onClick={onDelete}>
+            delete
+          </i>
+        </a>
+      </div>
+    </li>
+  );
+};
+
+TechItem.propTypes = {
+  tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { deleteTech }
+)(TechItem);
